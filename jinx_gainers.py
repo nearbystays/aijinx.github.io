6530e296c348c80a3d00e606a7abc5fdf72db171
nearbystays <86144003+nearbystays@users.noreply.github.com>
2024-03-20 01:53:16 +0000

    Pending changes exported from your codespace

--- jinx_gainers.py
+++ jinx_gainers.py
@@ -3,10 +3,12 @@ from yahoo_fin import options
 import pandas as pd
 
 # Get the data for the percent gainers
-gainers = si.get_day_gainers()
+gainers = si.get_data('gainers')
 
 # Print the data
 #Convert to table format using pandas
 df = pd.DataFrame(gainers)
+# Count
 print(df.head(10))
 print(df.tail(10))
+print(df.count())
